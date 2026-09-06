478bb3399c7cac2b98766df8aaed0160ad662261
AdarWa <adar.wasserman@gmail.com>
2026-09-06 22:53:48 +0300

    Add basic auth ui

--- app/src/main/java/net/adarw/bettersmartschool/settings/SettingsView.kt
+++ app/src/main/java/net/adarw/bettersmartschool/settings/SettingsView.kt
@@ -1,11 +1,18 @@
 package net.adarw.bettersmartschool.settings
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.jamal.composeprefs3.ui.PrefsScreen
-import com.jamal.composeprefs3.ui.prefs.ListPref
 import com.jamal.composeprefs3.ui.prefs.SwitchPref
+import net.adarw.bettersmartschool.auth.LoginWebView
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -21,5 +28,23 @@ fun SettingsScreen(prefs: AppPreferences) {
                 )
             }
         }
+    }
+    var showWebView by remember { mutableStateOf(false) }
+    if(showWebView){
+        LoginWebView("https://webtop.smartschool.co.il/account/login") {
+            showWebView = false
+            prefs.uniqueId.value = it.uniqueId
+            prefs.webToken.value = it.webToken
+        }
+    }else {
+        Button({showWebView = true}) {
+            Text("Authenticate")
+        }
+    }
+
+    if(prefs.isAuthenticated) {
+        Text("Credentials Present")
+    }else {
+        Text("Please Authenticate", color = Color.Red)
     }
 }
